ba7c2a035dac17949fcaf6ae275e251fdcb9f293
Ismael <ismael.js1996@gmail.com>
2021-10-03 12:12:07 -0300

    criação da index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let nomeEstudante = "Helena";
+console.log(nomeEstudante);
+
+const nomeDoEstudante = "Helena";
+console.log(nomeEstudante);
+
+let listaDeEstudante = ["Helena","Chico", "Mário"];
+let quantidadeDeEstudantes = listaDeEstudante.length;
+console.log(quantidadeDeEstudantes);
+
+let listaDeEstudantes1 = ["Helena","Chico", "Mário"];
+let quantidadeDeEstudantes1 = listaDeEstudantes1.length;
+for (let indice = 0; indice < quantidadeDeEstudantes1; indice++){
+    const alunoCorrente = listaDeEstudantes1[indice];
+    console.log(alunoCorrente);
+}
