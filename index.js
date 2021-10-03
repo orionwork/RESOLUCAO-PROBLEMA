9cf228e398e807aba1597cae40576538249efe25
Ismael <ismael.js1996@gmail.com>
2021-10-03 12:43:10 -0300

    adição do FOR, DO e While, usando o FOR  pra contagem

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ console.log(nomeEstudante);
 const nomeDoEstudante = "Helena";
 console.log(nomeEstudante);
 
+// **** Contagem do Array ****
 let listaDeEstudante = ["Helena","Chico", "Mário"];
 let quantidadeDeEstudantes = listaDeEstudante.length;
 console.log(quantidadeDeEstudantes);
 
-let listaDeEstudantes1 = ["Helena","Chico", "Mário"];
+// **** Rotina FOR ****
+let listaDeEstudantes1 = ["Helena","Chico", "Mário", "FIM"];
 let quantidadeDeEstudantes1 = listaDeEstudantes1.length;
 for (let indice = 0; indice < quantidadeDeEstudantes1; indice++){
     const alunoCorrente = listaDeEstudantes1[indice];
     console.log(alunoCorrente);
+}
+
+// **** Rotina DO e WHILE **** (aproveitamento codigo anterior)
+let inicio = 0;
+do {
+    console.log(listaDeEstudantes1[inicio]);
+    inicio++;
+} while (inicio < quantidadeDeEstudantes1);
+
+while (inicio < quantidadeDeEstudantes1){
+    console.log(listaDeEstudantes1[inicio]);
+    inicio++;
+}
+
+//  **** contagem até 10 com o FOR ****
+for (let numero = 1; numero <= 10; numero++){
+    console.log(numero);
 }
